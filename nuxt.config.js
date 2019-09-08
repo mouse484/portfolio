467644c70a0700d9ad711be7610018a3edb4e031
mouse_484 <escmouse484@gmail.com>
2019-09-08 06:39:10 +0000

    ✨ : Set nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,22 +4,16 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "mouse's Portfolio.",
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: '#9cc2e2' },
   /*
    ** Global CSS
    */
