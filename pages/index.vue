a4ffbbc126d4f9cd485b768ed07555e90f02538f
mouse_484 <escmouse484@gmail.com>
2020-02-05 09:54:12 +0000

    add about

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>
-    <div class="text-center">
-      <h1 id="title" class="mt-3">mouse's Portfolio.</h1>
-      <h3 class="mt-3">Student web and robot engineer.</h3>
+  <div class="px-5">
+    <h1 id="title" class="text-center mt-3">mouse's Portfolio.</h1>
+
+    <div id="about" class="mt-5">
+      <h2>About Me</h2>
+      <p class="h5 mt-1">
+        I am a Web and Robot engineer from Japan. I am self-taught in web
+        programs such as Node.js/Vue.js/TypeScript and so on. I dropped out of
+        junior high school and spent time on programs instead of studying.
+        However, I need to study general education, so I will study at an online
+        high school from April 2020. Please wait because it will be an
+        unbelievable engineer.
+      </p>
     </div>
 
     <skills class="mt-5" />
@@ -23,5 +32,12 @@ h1#title {
   color: #9cc2e2;
   font-family: 'Merienda', cursive;
   font-size: 8vh;
+}
+h2 {
+  border-bottom: solid 2px yellow;
+  width: 10rem;
+}
+#about > p {
+  line-height: 1.7;
 }
 </style>
