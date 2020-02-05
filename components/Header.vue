739232ef46ac147492500de0931ba0b7170e390c
mouse_484 <escmouse484@gmail.com>
2020-02-05 08:56:34 +0000

    add Header

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <b-navbar type="dark" class="mt-5">
+    <b-navbar-nav class="mx-auto">
+      <b-nav-item href="#">Home</b-nav-item>
+      <b-nav-item href="#about">About Me</b-nav-item>
+      <b-nav-item href="#skills">Skills</b-nav-item>
+      <b-nav-item href="#works">Works</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
+    <Header />
     <nuxt />
   </div>
 </template>
+
+<script>
+import Header from '../components/Header'
+export default { components: { Header } }
+</script>
 
 <style>
 body {
