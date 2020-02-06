68465541c2d8611b8a2b8df3b409315ffed34173
mouse_484 <escmouse484@gmail.com>
2020-02-06 02:11:08 +0000

    transition header

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="px-5">
-    <h1 id="title" class="text-center mt-3">mouse's Portfolio.</h1>
-
-    <div id="about" class="mt-5">
+    <div id="about" ref="about" class="mt-5">
       <h2>About Me</h2>
       <p class="h5 mt-1">
         I am a Web and Robot engineer from Japan. I am self-taught in web
@@ -29,15 +27,12 @@
 <script>
 import Skills from '../components/Skills.vue'
 import Works from '../components/Works.vue'
-export default { components: { Skills, Works } }
+export default {
+  components: { Skills, Works }
+}
 </script>
 
 <style scoped>
-h1#title {
-  color: #9cc2e2;
-  font-family: 'Merienda', cursive;
-  font-size: 8vh;
-}
 h2 {
   border-bottom: solid 2px yellow;
   width: 10rem;
