58f02381ecfdbe0a3a30f48634fd5a1bd2d66776
mouse_484 <escmouse484@gmail.com>
2020-02-06 03:14:10 +0000

    fix Layout

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,10 +1,18 @@
 <template>
   <b-row>
-    <b-col v-for="(skill, index) in skills" :key="index" style="width: 10rem;">
+    <b-col v-for="(skill, index) in skills" :key="index">
       <Card :name="skill[0]" :level="skill[1]" />
     </b-col>
   </b-row>
 </template>
+
+<style scoped>
+.col {
+  margin: 0 auto;
+  max-width: 10rem;
+  min-width: 10rem;
+}
+</style>
 
 <script>
 import skills from '~/assets/Skills.json'
