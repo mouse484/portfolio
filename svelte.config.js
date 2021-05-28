e51914279b3f002b1e2ef13bbda3df4f9723b4d0
mouse_484 <38714187+mouse484@users.noreply.github.com>
2021-05-28 00:02:31 +0000

    add @sveltejs/adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,17 @@
 import preprocess from 'svelte-preprocess';
 import WindiCSS from 'vite-plugin-windicss/dist/index.mjs';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			plugins: [WindiCSS()]
-		}
-	}
+  preprocess: preprocess(),
+  kit: {
+    target: '#svelte',
+    adapter: vercel(),
+    vite: {
+      plugins: [WindiCSS()],
+    },
+  },
 };
 
 export default config;
