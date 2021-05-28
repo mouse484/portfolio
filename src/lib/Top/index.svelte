93eb178b4a35ae0ab0d23f74066da0512fdde3b4
mouse_484 <38714187+mouse484@users.noreply.github.com>
2021-05-28 07:36:02 +0000

    fix animation

--- src/lib/Top/index.svelte
+++ src/lib/Top/index.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import Links from './Links.svelte';
+
+  let mounted = false;
+
+  onMount(() => (mounted = true));
 </script>
 
 <h1
-  class="text-4xl font-bold text-center md:text-7xl animate-animated animate-fadeInDownBig"
+  class="text-4xl font-bold text-center md:text-7xl animate-fadeInDownBig"
+  class:animation={mounted}
 >
   Hi, I'm Mouse
 </h1>
 <h2
-  class="pb-2 mt-8 text-2xl text-center border-b-4 border-blue-200 animate-animated animate-backInLeft animate-delay"
+  class="pb-2 mt-8 text-2xl text-center border-b-4 border-blue-200 animate-backInLeft animate-delay"
+  class:animation={mounted}
 >
   I'm a student in Japan.
 </h2>
 <div
-  class="mt-4 text-center animate-animated animate-backInUp animate-delay-1s"
+  class="mt-4 text-center animate-backInUp animate-delay-1s"
+  class:animation={mounted}
 >
   <Links />
 </div>
+
+<style>
+  .animation {
+    @apply animate-animated;
+  }
+</style>
