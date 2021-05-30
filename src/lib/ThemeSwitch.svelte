80c4651d5bc3ef27678746d910a601961aa77b9e
mouse_484 <38714187+mouse484@users.noreply.github.com>
2021-05-30 09:14:03 +0000

    Match media

--- src/lib/ThemeSwitch.svelte
+++ src/lib/ThemeSwitch.svelte
@@ -6,16 +6,25 @@
   // });
   import Icon from '$lib/Icon.svelte';
   import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+  import { onMount } from 'svelte';
 
   import { writable } from 'svelte/store';
 
   export const theme = writable<'light' | 'dark'>('light');
 
+  onMount(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      theme.set('dark');
+    }
+  });
+
   $: {
     if (typeof document !== 'undefined') {
-      $theme === 'light'
-        ? document.querySelector('html')?.classList.remove('dark')
-        : document.querySelector('html')?.classList.add('dark');
+      if ($theme === 'dark') {
+        document.querySelector('html')?.classList.add('dark');
+      } else {
+        document.querySelector('html')?.classList.remove('dark');
+      }
     }
   }
 </script>
