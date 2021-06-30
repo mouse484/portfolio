98d21db533706ee56ddef8d0b0d123991f10a3e3
mouse_484 <escmouse484@gmail.com>
2021-06-30 11:06:17 +0000

    border spacing

--- src/lib/Top/index.svelte
+++ src/lib/Top/index.svelte
@@ -14,13 +14,13 @@
   Hi, I'm Mouse
 </h1>
 <h2
-  class="pb-2 mt-8 text-2xl text-center border-b-4 border-blue-200 animate-backInLeft animate-delay"
+  class="pb-4 mt-8 text-2xl text-center border-b-4 border-blue-200 animate-backInLeft animate-delay"
   class:animation={mounted}
 >
   I'm a student in Japan.
 </h2>
 <div
-  class="mt-4 text-center animate-backInUp animate-delay-1s"
+  class="mt-8 text-center animate-backInUp animate-delay-1s"
   class:animation={mounted}
 >
   <Links />
