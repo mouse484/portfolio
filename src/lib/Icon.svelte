8a837c5afba2424329f057e9e03ffb61a997c5b7
mouse_484 <escmouse484@gmail.com>
2022-06-02 05:39:55 +0000

    feat: :sparkles: Icon component to fontawesome v6

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
+  export const ssr = false;
   import type { IconDefinition } from '@fortawesome/fontawesome-common-types';
 
-  import Fa from 'svelte-fa';
+  // https://fontawesome.com/docs/web/style/size
+  const size = { lg: '1.25em' };
+
   export let icon: IconDefinition;
+  const {
+    icon: [width, height, , _unicode, d],
+  } = icon;
 </script>
 
-<Fa {icon} size="lg" />
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 {width} {height}"
+  width={size.lg}
+  height={size.lg}
+  class="fill-black dark:fill-white"
+>
+  <path d={typeof d === 'string' ? d : d.join('')} />
+</svg>
