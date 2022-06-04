86eefece6f083bbc5ea92866f7c0b68eb75b5c63
mouse_484 <escmouse484@gmail.com>
2022-06-04 09:29:43 +0000

    feat: meke load screen

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,28 @@
-<script>
+<script lang="ts">
   import 'virtual:windi.css';
   import '../app.css';
+  import { onMount } from 'svelte';
+  import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+
+  export let onLoad = false;
+
+  onMount(() => {
+    onLoad = true;
+  });
 
   import ThemeSwitch from '$lib/ThemeSwitch.svelte';
+  import Icon from '$lib/Icon.svelte';
 </script>
 
-<div class="dark:bg-dark dark:text-gray-50">
-  <header class="float-right m-8">
-    <ThemeSwitch />
-  </header>
-  <slot />
-</div>
+{#if onLoad}
+  <div class="dark:bg-dark dark:text-gray-50">
+    <header class="float-right m-8">
+      <ThemeSwitch />
+    </header>
+    <slot />
+  </div>
+{:else}
+  <div class="h-screen flex justify-center items-center">
+    <Icon icon={faCircleNotch} className="animate-spin" />
+  </div>
+{/if}
