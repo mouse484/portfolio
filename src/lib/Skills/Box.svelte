b7d71fcd243f38a00e0a9a4b287262817d2d306d
mouse_484 <escmouse484@gmail.com>
2022-09-05 07:17:24 +0000

    feat: 階層分け

---
+++ src/lib/Skills/Box.svelte
@@ -0,0 +1,16 @@
+<script>
+  import SimpleIcon from '$lib/SimpleIcon.svelte';
+
+  export let name = '';
+  export let skills = {};
+</script>
+
+<div class="p-5 rounded border-blue-400 border-3">
+  <SimpleIcon {name} />
+  <div>{name}</div>
+  {#if Object.keys(skills).length}
+    {#each Object.entries(skills) as [skillName, inSkills]}
+      <svelte:self name={skillName} skills={inSkills} />
+    {/each}
+  {/if}
+</div>
--- src/lib/Skills/index.svelte
+++ src/lib/Skills/index.svelte
@@ -1,65 +1,14 @@
 <script lang="ts">
-  import SimpleIcon from '$lib/SimpleIcon.svelte';
-
-  const skills = [
-    'JavaScript',
-    'TypeScript',
-    'Node.js',
-    'npm',
-    'yarn',
-    'Vue.js',
-    'Nuxt.js',
-    'Svelte',
-    'React',
-    'Next.js',
-    'Angular',
-    'Gatsby',
-    'jQuery',
-    'Babel',
-    'Webpack',
-    'rollup.js',
-    'gulp',
-    'Vite',
-    'Express',
-    'Fastify',
-    'Electron',
-    'ESLint',
-    'Prettier',
-    'Jest',
-    'Mocha',
-    'HTML5',
-    'CSS3',
-    'Bootstrap',
-    'Tailwind CSS',
-    'PostCSS',
-    'Vercel',
-    'Netlify',
-    'Go',
-    'Python',
-    'Ruby',
-    'C',
-    'C++',
-    'Arduino',
-    'Docker',
-    'Git',
-    'GitHub',
-    'GitLab',
-    'Visual Studio Code',
-    'Gitpod',
-    'Slack',
-    'Discord',
-  ];
+  import Box from './Box.svelte';
+  import skills from './skills';
 </script>
 
 <section>
   <h3 class="text-3xl text-center">My Skills</h3>
 
   <div class="flex flex-wrap gap-4 justify-center px-8 mx-auto mt-8 max-w-2xl">
-    {#each skills as skill}
-      <div class="flex space-x-2">
-        <SimpleIcon name={skill} />
-        <p>{skill}</p>
-      </div>
+    {#each Object.entries(skills) as [skillName, inSkills]}
+      <Box name={skillName} skills={inSkills} />
     {/each}
   </div>
 </section>
