d1b88482630dd204a1e130a7ae4b6000216ac8cd
mouse_484 <escmouse484@gmail.com>
2022-09-05 08:21:35 +0000

    feat: skill list

--- src/lib/Skills/skills.ts
+++ src/lib/Skills/skills.ts
@@ -1,3 +1,69 @@
-export default {
-  JavaScript: { TypeScript: {}, 'Node.js': { npm: {}, yarn: {} } },
+const skills = {
+  JavaScript: {
+    TypeScript: {},
+    'Node.js': { npm: {}, yarn: {} },
+    Web: {
+      React: { 'Next.js': {}, Gatsby: {} },
+      'Vue.js': { 'Nuxt.js': {} },
+      Svelte: { SvelteKit: { _icon: 'svelte' } },
+      Angular: {},
+      jQuery: {},
+    },
+    Bundler: {
+      Webpack: {},
+      Babel: {},
+      Vite: {},
+      'rollup.js': {},
+      gulp: {},
+      esbuild: {},
+    },
+    Server: {
+      Express: {},
+      Fastify: {},
+    },
+    Application: {
+      Electron: {},
+    },
+    'linter/formatter': {
+      ESLint: {},
+      Prettier: {},
+    },
+    test: {
+      Jest: {},
+      Mocha: {},
+    },
+  },
+  HTML: { _icon: 'HTML5' },
+  CSS: {
+    _icon: 'CSS3',
+    Bootstrap: {},
+    'Tailwind CSS': {},
+    PostCSS: {},
+  },
+  Hosting: {
+    Vercel: {},
+    Netlify: {},
+    'GitHub Pages': {},
+    heroku: {},
+  },
+  Go: {},
+  Python: {},
+  Ruby: {},
+  C: {},
+  'C++': {},
+  R: {},
+  Arduino: {},
+  Git: {
+    GitHub: {},
+    GitLab: {},
+  },
+  Editor: {
+    'Visual Studio Code': { Gitpod: {} },
+  },
+  Chat: {
+    Discord: {},
+    Slack: {},
+  },
 };
+
+export default skills;
