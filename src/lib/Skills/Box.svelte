954c285a25b1663cd9dd7fa1faed1837c543457c
mouse_484 <escmouse484@gmail.com>
2022-09-05 07:58:13 +0000

    feat: 名前とアイコンが違うのを調整

--- src/lib/Skills/Box.svelte
+++ src/lib/Skills/Box.svelte
@@ -1,14 +1,21 @@
-<script>
+<script lang="ts">
   import SimpleIcon from '$lib/SimpleIcon.svelte';
 
   export let name = '';
   export let skills = {};
+  let icon = name;
+
+  $: {
+    if (skills.hasOwnProperty('_icon')) {
+      icon = (skills as { _icon: string })._icon || name;
+    }
+  }
 </script>
 
 <div class="p-5 rounded border-blue-400 border-3">
-  <SimpleIcon {name} />
+  <SimpleIcon name={icon} />
   <div>{name}</div>
-  {#if Object.keys(skills).length}
+  {#if Object.keys(skills).length > (skills.hasOwnProperty('_icon') ? 1 : 0)}
     {#each Object.entries(skills) as [skillName, inSkills]}
       <svelte:self name={skillName} skills={inSkills} />
     {/each}
