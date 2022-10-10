50506ebc104182dc3d0b45550954841e8981dc07
mouse_484 <escmouse484@gmail.com>
2022-10-10 11:43:26 +0000

    header sticky

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -3,7 +3,9 @@
   import ThemeSwitch from '$lib/ThemeSwitch.svelte';
 </script>
 
-<header class="float-right m-8 flex gap-4">
+<header
+  class="float-right m-8 flex gap-4 sticky top-6 bg-white dark:bg-dark z-10 px-4 rounded"
+>
   <ScrollTo to="about">About Me</ScrollTo>
   <ScrollTo to="works">My Works</ScrollTo>
   <ScrollTo to="skills">My Skills</ScrollTo>
