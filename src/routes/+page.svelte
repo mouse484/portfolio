27bcae511356b4ae1fc99b420a941ff5e5021039
mouse_484 <escmouse484@gmail.com>
2022-10-13 15:09:05 +0000

    page:top->hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
+  import Hero from '$lib/templates/page/Hero.svelte';
   import About from '$lib/templates/page/About.svelte';
   import Works from '$lib/templates/page/Works.svelte';
   import Skills from '$lib/templates/page/Skills.svelte';
   import Posts from '$lib/templates/page/Posts.svelte';
-  import MoveArrow from '$lib/MoveArrow.svelte';
-  import Top from '$lib/Top/index.svelte';
 </script>
 
 <svelte:head>
@@ -12,12 +11,9 @@
   <html lang="ja" />
 </svelte:head>
 
-<div class="h-screen pt-[30vh]">
-  <Top />
-  <div class="mt-48">
-    <MoveArrow to="main" />
-  </div>
-</div>
+<section class="h-screen pt-[30vh]">
+  <Hero />
+</section>
 
 <section id="main" class="py-10 space-y-20">
   <About />
