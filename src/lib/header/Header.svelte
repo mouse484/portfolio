13c9ea138b35b28002021f44df89ce6bf8171ad1
mouse_484 <escmouse484@gmail.com>
2022-10-13 13:30:52 +0000

    remove old code

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,17 +1,3 @@
-<!-- <script lang="ts">
-  import ScrollTo from './ScrollTo.svelte';
-  import ThemeSwitch from '$lib/ThemeSwitch.svelte';
-</script>
-
-<header
-  class="float-right m-8 flex gap-4 sticky top-6 bg-white dark:bg-dark z-10 px-4 rounded"
->
-  <ScrollTo to="about">About Me</ScrollTo>
-  <ScrollTo to="works">My Works</ScrollTo>
-  <ScrollTo to="skills">My Skills</ScrollTo>
-  <ScrollTo to="posts">My Posts</ScrollTo>
-  <ThemeSwitch />
-</header> -->
 <script lang="ts">
   import ScrollTo from './ScrollTo.svelte';
   import ThemeSwitch from '$lib/ThemeSwitch.svelte';
