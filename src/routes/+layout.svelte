0269834db0063e07cae3cafa0734fcb85e9e9e34
mouse_484 <escmouse484@gmail.com>
2022-10-15 05:00:43 +0000

    topへ戻る

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
   import Header from '$lib/templates/Header.svelte';
   import Loading from '$lib/components/element/Loading.svelte';
   import Footer from '$lib/templates/Footer.svelte';
+  import ScrollToTop from '$lib/components/modules/ScrollToTop.svelte';
   export let onLoad = false;
 
   onMount(() => {
@@ -14,8 +15,11 @@
 {#if onLoad}
   <div class="background-color">
     <Header />
-    <slot />
+    <div id="top">
+      <slot />
+    </div>
     <Footer />
+    <ScrollToTop />
   </div>
 {:else}
   <Loading />
