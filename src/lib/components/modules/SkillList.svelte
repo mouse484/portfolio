db983bb70b0f3710eec7534aff52aec7a3216eda
mouse_484 <escmouse484@gmail.com>
2022-10-30 09:43:52 +0000

    skillListを自作cssに

--- src/lib/components/modules/SkillList.svelte
+++ src/lib/components/modules/SkillList.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-  import { Button, Dropdown, DropdownItem, Chevron } from 'flowbite-svelte';
   import SimpleIcon from '$lib/components/element/SimpleIcon.svelte';
   import Icon from '$lib/components/element/Icon.svelte';
-  import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
+  import {
+    faEllipsis,
+    faChevronDown,
+    faChevronRight,
+  } from '@fortawesome/free-solid-svg-icons';
 
   export let skills: { [key: string]: {} };
   export let name: string = '';
@@ -12,6 +15,7 @@
   let useSkills: { [key: string]: {} };
   let icon = name;
   let iconStatus = true;
+  let vis = false;
 
   const isNested = (skills: typeof useSkills) =>
     Object.keys(skills).length > (skills.hasOwnProperty('_icon') ? 1 : 0);
@@ -28,32 +32,38 @@
 </script>
 
 {#if name}
-  <Button
-    color="alternative"
-    class="flex-1 !justify-start !items-start m-1 w-full outline-none !text-start"
-  >
-    {#if iconStatus}
-      <SimpleIcon name={icon} size={18} bind:status={iconStatus} />
-    {:else}
-      <Icon icon={faEllipsis} />
-    {/if}
-    <span class="pl-2">{name}</span>
-    {#if nested}
-      <span class="ml-auto">
-        <Chevron placement={isRoot ? 'bottom' : 'right'} />
-      </span>
-    {/if}
-  </Button>
+  <div class="relative">
+    <button
+      class="flex flex-nowrap items-center p-2 m-1 min-w-min rounded-md border grow"
+      on:click={() => (vis = !vis)}
+    >
+      {#if iconStatus}
+        <SimpleIcon name={icon} size={18} bind:status={iconStatus} />
+      {:else}
+        <Icon icon={faEllipsis} />
+      {/if}
+      <span class="px-2">{name}</span>
+      {#if nested}
+        <span class="ml-auto">
+          <Icon icon={isRoot ? faChevronDown : faChevronRight} size="sm" />
+        </span>
+      {/if}
+    </button>
 
-  {#if nested}
-    <Dropdown placement={isRoot ? 'bottom' : 'right'} class="z-50">
-      {#each Object.entries(useSkills) as [skillName, next]}
-        <DropdownItem class="background-color">
-          <svelte:self name={skillName} skills={next} nested={isNested(next)} />
-        </DropdownItem>
-      {/each}
-    </Dropdown>
-  {/if}
+    {#if nested}
+      <div class="absolute top-14 z-50 {vis ? 'visible' : 'invisible'}">
+        {#each Object.entries(useSkills) as [skillName, next]}
+          <div class="background-color">
+            <svelte:self
+              name={skillName}
+              skills={next}
+              nested={isNested(next)}
+            />
+          </div>
+        {/each}
+      </div>
+    {/if}
+  </div>
 {:else}
   <!-- init -->
   {#each Object.entries(useSkills) as [skillName, next]}
