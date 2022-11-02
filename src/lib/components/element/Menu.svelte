d904a04d0f8e22767dc573a91480468e580ae951
mouse_484 <escmouse484@gmail.com>
2022-11-02 11:09:02 +0000

    スマホサイズではヘッダーはメニューになる

---
+++ src/lib/components/element/Menu.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Icon from '$lib/components/element/Icon.svelte';
+  import { faBars } from '@fortawesome/free-solid-svg-icons';
+  import ScrollTo from '$lib/components/element/ScrollTo.svelte';
+
+  let visable = false;
+  export let items: { [key: string]: string };
+</script>
+
+<div class="flex-none">
+  <button on:click={() => (visable = !visable)}>
+    <Icon icon={faBars} />
+  </button>
+  {#if visable}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div
+      class="absolute z-50 rounded-md dark:bg-zinc-700"
+      on:click={() => (visable = false)}
+    >
+      {#each Object.entries(items) as [to, name]}
+        <div class="p-1 m-1 rounded-md border border-stone-500">
+          <ScrollTo class="px-3" {to}>
+            <p class="text-justify">{name}</p>
+          </ScrollTo>
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div>
--- src/lib/templates/Header.svelte
+++ src/lib/templates/Header.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-  import ScrollTo from '../components/element/ScrollTo.svelte';
+  import Menu from '$lib/components/element/Menu.svelte';
+  import ScrollTo from '$lib/components/element/ScrollTo.svelte';
   import ThemeSwitch from '$lib/components/element/ThemeSwitch.svelte';
+  import Responsive from '$lib/components/modules/Responsive.svelte';
 
   const naviList = {
     about: 'About Me',
@@ -13,11 +15,16 @@
 <header
   class="flex sticky top-0 z-50 items-center px-2 py-4 border-b background-color"
 >
-  <h1 class="text-2xl">mouse's Portfolio</h1>
+  <Responsive breakpoint="sm" visable>
+    <Menu items={naviList} />
+  </Responsive>
+  <h1 class="ml-2 text-2xl">mouse's Portfolio</h1>
   <div class="flex mr-4 ml-auto">
-    {#each Object.entries(naviList) as [to, name]}
-      <ScrollTo class="px-3" {to}>{name}</ScrollTo>
-    {/each}
+    <Responsive breakpoint="sm">
+      {#each Object.entries(naviList) as [to, name]}
+        <ScrollTo class="px-3" {to}>{name}</ScrollTo>
+      {/each}
+    </Responsive>
     <ThemeSwitch />
   </div>
 </header>
