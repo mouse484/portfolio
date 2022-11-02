1494c39e5e742d4f251a3e273e26851ebabcc718
mouse_484 <escmouse484@gmail.com>
2022-11-02 10:37:35 +0000

    make Responsive component

---
+++ src/lib/components/modules/Responsive.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  // ref: https://tailwindcss.com/docs/responsive-design
+  const breakpointList = {
+    sm: '(min-width: 640px)',
+    lg: '(min-width: 1024px)',
+  };
+
+  export let breakpoint: keyof typeof breakpointList;
+  export let visable = false;
+
+  let result = false;
+
+  $: {
+    const matches = window.matchMedia(breakpointList[breakpoint]).matches;
+    result = visable ? !matches : matches;
+  }
+</script>
+
+{#if result}
+  <slot />
+{/if}
