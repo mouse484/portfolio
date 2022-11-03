df2ca68c9707acbf43b1a2565e2636021f77a662
mouse_484 <escmouse484@gmail.com>
2022-11-03 13:07:45 +0000

    Postsをレスポンシブに

--- src/lib/templates/page/Posts.svelte
+++ src/lib/templates/page/Posts.svelte
@@ -17,25 +17,29 @@
 <section id="posts">
   <SectionName>My Posts</SectionName>
 
-  <div class="max-w-lg mx-auto mt-8">
+  <div class="mx-auto mt-8 max-w-lg">
     {#await fetchPosts}
-      <p>...waiting</p>
+      <p class="text-center">loading...</p>
     {:then data}
       {#each data as { platform, title, link, isoDate }}
-        <div class="mt-2 text-sm">
-          {new Date(isoDate).toLocaleDateString()}
-        </div>
+        <div class="mx-4">
+          <div class="text-sm">
+            {new Date(isoDate).toLocaleDateString()}
+          </div>
 
-        <p class="flex content-start gap-4">
-          <span class="flex-initial">
-            <SimpleIcon name={platform} />
-          </span>
-          <span class="flex-grow text-left">
-            <Link href={link}>
-              <span class="truncate ...">{title}</span>
-            </Link>
-          </span>
-        </p>
+          <div class="flex gap-4">
+            <span class="flex-initial">
+              <SimpleIcon name={platform} />
+            </span>
+            <span class="min-w-0 text-left">
+              <Link href={link}>
+                <p class="truncate hover:break-words hover:whitespace-normal">
+                  {title}
+                </p>
+              </Link>
+            </span>
+          </div>
+        </div>
       {/each}
     {:catch error}
       {console.log(error)}
