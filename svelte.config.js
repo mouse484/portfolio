23db0506332143560e04fc7d6354f22fac3445aa
mouse_484 <escmouse484@gmail.com>
2022-12-17 02:07:11 +0000

    remove tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [preprocess({ postcss: true })],
+  preprocess: [preprocess()],
   kit: {
     adapter: vercel(),
   },
