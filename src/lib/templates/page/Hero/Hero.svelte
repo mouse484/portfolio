c438eec9e4a2663c3163cc3a12cbc7974ffd2b34
mouse_484 <escmouse484@gmail.com>
2022-12-17 03:05:04 +0000

    Hero & spacer

---
+++ src/lib/templates/page/Hero/Hero.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import MyLinks from '$lib/templates/page/Hero/MyLinks.svelte';
+  import MoveArrow from '$lib/components/element/MoveArrow.svelte';
+  import Spaser from '$lib/components/element/Spaser.svelte';
+
+  let mounted = false;
+
+  onMount(() => (mounted = true));
+</script>
+
+<section>
+  <h1>Hi, I'm Mouse</h1>
+  <h2>I'm a student in Japan.</h2>
+  <Spaser size={2} />
+  <MyLinks />
+  <Spaser size={2} />
+  <MoveArrow to="about" />
+</section>
+
+<style>
+  section {
+    text-align: center;
+  }
+</style>
