ea0a50fb2f99b63afedb0576d341ce2627500669
mouse_484 <escmouse484@gmail.com>
2022-12-17 10:05:56 +0000

    Hero ボーダ-

--- src/lib/templates/page/Hero/Hero.svelte
+++ src/lib/templates/page/Hero/Hero.svelte
@@ -11,15 +11,23 @@
 
 <section>
   <h1>Hi, I'm Mouse</h1>
-  <h2>I'm a student in Japan.</h2>
+  <h3>I'm a student in Japan.</h3>
   <Spaser size={2} />
   <MyLinks />
   <Spaser size={2} />
   <MoveArrow to="about" />
 </section>
 
-<style>
+<style lang="postcss">
   section {
     text-align: center;
+  }
+  h1 {
+    font-size: 4em;
+  }
+  h3 {
+    font-weight: normal;
+    padding: 1em;
+    border-bottom: 4px solid var(--sky-bule);
   }
 </style>
