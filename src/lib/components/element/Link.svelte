047990cea05605fdb9b5aa9fff84396ce5536db3
mouse_484 <escmouse484@gmail.com>
2022-12-18 04:19:18 +0000

    Link

--- src/lib/components/element/Link.svelte
+++ src/lib/components/element/Link.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
   export let href: string;
+  export let target = '';
+  export let rel = target === '__blank' ? 'noopener noreferrer' : '';
+  export let ariaLabel = '';
 </script>
 
-<a {href}>
+<a {href} {target} {rel} aria-label={ariaLabel}>
   <slot />
 </a>
 
 <style lang="postcss">
   a {
-    color: #0284c7;
+    color: var(--color, #0284c7);
     text-decoration: none;
     &:hover {
       text-decoration: underline;
