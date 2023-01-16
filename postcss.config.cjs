e9443f6f2b08b878fc7b08f79f57fbb82f3664a5
mouse_484 <escmouse484@gmail.com>
2023-01-16 04:42:52 +0000

    remove postscss

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-const autoprefixer = require('autoprefixer');
-const postcssNesting = require('postcss-nesting');
-
-const config = {
-  plugins: [postcssNesting(), autoprefixer],
-};
-
-module.exports = config;
