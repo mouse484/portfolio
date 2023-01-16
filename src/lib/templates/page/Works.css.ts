de6ec9ca77f4f7db8f22859d0a7375d855df7f57
mouse_484 <escmouse484@gmail.com>
2023-01-16 04:49:33 +0000

    fix works

--- src/lib/templates/page/Works.css.ts
+++ src/lib/templates/page/Works.css.ts
@@ -2,4 +2,8 @@ import { sprinkles } from '$lib/style/sprinkles.css';
 import { flex } from '$lib/style/utils.css';
 import { style } from '@vanilla-extract/css';
 
-export const projects = style([flex, sprinkles({ gap: 'small' })]);
+export const projects = style([
+  flex,
+  { justifyContent: 'center' },
+  sprinkles({ gap: 'small' }),
+]);
